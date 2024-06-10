b539e1b36f61099228d649f431dbd2ed57123acf
javier <116373478+JavierBotell4@users.noreply.github.com>
2024-06-10 22:06:16 +0200

    Cambio en las Reedireciones de Login Y Registro

--- lib/screens/register.dart
+++ lib/screens/register.dart
@@ -1,165 +1,144 @@
 import 'package:flutter/material.dart';
-import 'package:list_movies_series/screens/home.dart';
-import 'package:list_movies_series/screens/register.dart';
-import 'package:list_movies_series/google_sign_in/google_sign_in.dart';
 
-class LoginPage extends StatefulWidget {
-  @override
-  _LoginPageState createState() => _LoginPageState();
-}
-
-class _LoginPageState extends State<LoginPage> {
-  bool isChecked = false;
-
+class RegisterPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: null,
-      body: SingleChildScrollView(
-        child: Container(
-          decoration: BoxDecoration(
-            gradient: LinearGradient(
-              begin: FractionalOffset.topCenter,
-              end: FractionalOffset.bottomCenter,
-              colors: <Color>[
-                Color.fromRGBO(252, 92, 92, 1),
-                Color.fromRGBO(252, 68, 172, 1),
-                Color.fromRGBO(228, 84, 244, 1),
-                Color.fromRGBO(156, 84, 244, 1),
-              ],
-            ),
-          ),
-          padding: EdgeInsets.all(20.0),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Column(
-                children: [
-                  Container(
-                    margin: EdgeInsets.only(top: 20),
-                    child: CircleAvatar(
-                      backgroundImage: AssetImage('assets/images/logo.png'),
-                      radius: 60,
-                      child: Container(
-                        alignment: Alignment.topCenter,
-                        decoration: BoxDecoration(
-                          border: Border.all(
-                            color: Color.fromARGB(255, 218, 216, 216),
-                            width: 4.0,
-                          ),
-                          borderRadius: BorderRadius.circular(60),
-                        ),
-                      ),
-                    ),
-                  ),
-                  Container(
-                    width: 400.0,
-                    margin: EdgeInsets.only(bottom: 5.0),
-                    padding: EdgeInsets.all(20.0),
-                    child: Column(
-                      children: [
-                        Container(
-                          child: TextField(
-                            decoration: InputDecoration(
-                              labelText: 'Correo electrónico',
-                              labelStyle:
-                                  TextStyle(color: Colors.white, fontSize: 15),
-                            ),
-                            style: TextStyle(color: Colors.white),
-                          ),
-                        ),
-                        Container(
-                          child: TextField(
-                            obscureText: true,
-                            decoration: InputDecoration(
-                              labelText: 'Contraseña',
-                              labelStyle: TextStyle(color: Colors.white),
-                            ),
-                            style: TextStyle(color: Colors.white),
-                          ),
-                        ),
-                      ],
-                    ),
-                  ),
-                  Container(
-                    margin: EdgeInsets.only(left: 50, bottom: 10),
-                    child: Column(
-                      children: [
-                        Align(
-                          alignment: Alignment.centerRight,
-                          child: Text(
-                            '¿Se Olvidó de la Contraseña?',
-                            style: TextStyle(
-                                color: Color.fromARGB(250, 104, 58, 183),
-                                fontSize: 15),
-                          ),
-                        ),
-                        Row(
-                          children: [
-                            Container(
-                              padding: EdgeInsets.all(10.0),
-                              child: Row(
-                                children: [
-                                  Checkbox(
-                                    value: isChecked,
-                                    checkColor: Colors
-                                        .white, // Color del check cuando está activo
-                                    activeColor: isChecked
-                                        ? Colors.purple
-                                        : Colors
-                                            .transparent, // Color del fondo del checkbox cuando está activo o transparente cuando está desactivado
-                                    onChanged: (value) {
-                                      setState(() {
-                                        isChecked = value!;
-                                      });
-                                    },
-                                  ),
-                                  Text(
-                                    'Recordar contraseña',
-                                    style: TextStyle(
-                                        color: Colors.white, fontSize: 15),
-                                  ),
-                                ],
-                              ),
-                            ),
-                          ],
-                        ),
-                      ],
-                    ),
-                  ),
-                  SizedBox(width: 20.0),
-                  ElevatedButton(
-                    onPressed: () {
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(builder: (context) => Home()),
-                      );
-                    },
-                    child: Text('Iniciar sesión'),
-                  ),
-                  SizedBox(height: 20.0),
-                  ElevatedButton(
-                    onPressed: () {},
-                    child: Text('Iniciar sesión con Google'),
-                  ),
-                  SizedBox(height: 20.0),
-                  GestureDetector(
-                    onTap: () {
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(builder: (context) => LoginPage()),
-                      );
-                    },
-                    child: Container(
-                      margin: EdgeInsets.only(
-                          bottom: 60), // Margen solo hacia abajo
-                      child: Text('Registrarse'),
-                    ),
-                  ),
-                ],
-              ),
+      body: Container(
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            begin: FractionalOffset.topCenter,
+            end: FractionalOffset.bottomCenter,
+            colors: <Color>[
+              Color.fromRGBO(252, 92, 92, 1),
+              Color.fromRGBO(252, 68, 172, 1),
+              Color.fromRGBO(228, 84, 244, 1),
+              Color.fromRGBO(156, 84, 244, 1),
             ],
           ),
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            SizedBox(height: 20),
+            Expanded(
+              child: SingleChildScrollView(
+                child: Container(
+                  margin: EdgeInsets.only(top: 20, left: 100.0, right: 100.0),
+                  child: Column(
+                    children: [
+                      Container(
+                        margin: EdgeInsets.only(bottom: 50),
+                        child: CircleAvatar(
+                          backgroundImage: AssetImage('assets/images/logo.png'),
+                          radius: 60,
+                          child: Container(
+                            alignment: Alignment.topCenter,
+                            decoration: BoxDecoration(
+                              border: Border.all(
+                                color: Color.fromARGB(255, 218, 216, 216),
+                                width: 4.0,
+                              ),
+                              borderRadius: BorderRadius.circular(60),
+                            ),
+                          ),
+                        ),
+                      ),
+                      TextField(
+                        decoration: InputDecoration(
+                          labelText: 'Nick',
+                          labelStyle:
+                              TextStyle(color: Colors.white, fontSize: 10),
+                        ),
+                        style: TextStyle(
+                          color: Colors.white,
+                        ),
+                      ),
+                      TextField(
+                        decoration: InputDecoration(
+                          labelText: 'Nombre',
+                          labelStyle:
+                              TextStyle(color: Colors.white, fontSize: 10),
+                        ),
+                        style: TextStyle(
+                          color: Colors.white,
+                        ),
+                      ),
+                      TextField(
+                        decoration: InputDecoration(
+                          labelText: 'Apellidos',
+                          labelStyle:
+                              TextStyle(color: Colors.white, fontSize: 10),
+                        ),
+                        style: TextStyle(
+                          color: Colors.white,
+                        ),
+                      ),
+                      TextField(
+                        decoration: InputDecoration(
+                          labelText: 'Género',
+                          labelStyle:
+                              TextStyle(color: Colors.white, fontSize: 10),
+                        ),
+                        style: TextStyle(
+                          color: Colors.white,
+                        ),
+                      ),
+                      TextField(
+                        decoration: InputDecoration(
+                          labelText: 'Teléfono',
+                          labelStyle:
+                              TextStyle(color: Colors.white, fontSize: 10),
+                        ),
+                        style: TextStyle(
+                          color: Colors.white,
+                        ),
+                      ),
+                      TextField(
+                        decoration: InputDecoration(
+                          labelText: 'Correo electrónico',
+                          labelStyle:
+                              TextStyle(color: Colors.white, fontSize: 10),
+                        ),
+                        style: TextStyle(
+                          color: Colors.white,
+                        ),
+                      ),
+                      TextField(
+                        decoration: InputDecoration(
+                          labelText: 'Contraseña',
+                          labelStyle:
+                              TextStyle(color: Colors.white, fontSize: 10),
+                        ),
+                        style: TextStyle(
+                          color: Colors.white,
+                        ),
+                      ),
+                      TextField(
+                        decoration: InputDecoration(
+                          labelText: 'Confirmar Contraseña',
+                          labelStyle:
+                              TextStyle(color: Colors.white, fontSize: 10),
+                        ),
+                        style: TextStyle(
+                          color: Colors.white,
+                        ),
+                      ),
+                      SizedBox(height: 20),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                // Acción al presionar el botón de registrarse
+              },
+              child: Text('Registrarse'),
+            ),
+            SizedBox(height: 20),
+          ],
         ),
       ),
     );
